ff1c22b4b6eeff40e9e476422dd9d47179eef5de
nemi21 <48768261+nemi21@users.noreply.github.com>
2021-11-02 16:36:23 -0400

    export tilemap

--- src/Game.js
+++ src/Game.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 function gameLoop(){
-
+    console.log("game looop");
 }
 
 setInterval(gameLoop,1000.75);
---
+++ src/TileMap.js
@@ -0,0 +1,9 @@
+export default class TileMap{
+    constructor(tileSize){
+        this.tileSize = tileSize;
+    }
+
+    draw(){
+        console.log("draw");
+    }//end of draw
+}//end of export
