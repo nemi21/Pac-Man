84f60384906106982e360ffad2389fc2ba65ec76
nemi21 <48768261+nemi21@users.noreply.github.com>
2021-11-02 16:37:56 -0400

    comments

--- src/Game.js
+++ src/Game.js
@@ -3,6 +3,6 @@ const ctx = canvas.getContext("2d");
 
 function gameLoop(){
     console.log("game looop");
-}
+}//end of function gameloop
 
-setInterval(gameLoop,1000.75);
+setInterval(gameLoop, 1000 / 75);
