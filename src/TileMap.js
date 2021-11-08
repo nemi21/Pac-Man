13936925c16233b889dd96b8c2c52f171d16daff
nemi21 <48768261+nemi21@users.noreply.github.com>
2021-11-08 10:26:35 -0500

    Update TileMap.js

--- src/TileMap.js
+++ src/TileMap.js
@@ -31,13 +31,21 @@ export default class TileMap {
                 if(tile === 1){
                     this.#drawWall(ctx, column, row, this.tileSize);
                 }//end of if
+                else if(tile === 0){
+                    this.#drawDot(ctx, column, row, this.tileSize);
+                }//end of else if
             }//end of for
+
         }//end of for
 
     }//end of draw
 
+    #drawDot(ctx, column, row, size){
+        ctx.drawImage(this.yellowDot, column * this.tileSize, row * this.tileSize, size, size);
+    }
+
     #drawWall(ctx, column, row, size){
-        ctx. drawImage(this.wall, column * this.tileSize, row * this.tileSize, size, size);
+        ctx.drawImage(this.wall, column * this.tileSize, row * this.tileSize, size, size);
     }
         
         setCanvasSize(canvas) {
