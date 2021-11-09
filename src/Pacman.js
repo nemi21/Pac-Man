a8b5fcbb459cce9421953c716748f6efd09494c2
nemi21 <48768261+nemi21@users.noreply.github.com>
2021-11-09 13:00:11 -0500

    Update Pacman.js

--- src/Pacman.js
+++ src/Pacman.js
@@ -52,7 +52,10 @@ draw(ctx) {
     #keydown =(event)=>{
         //up
         if(event.keyCode == 38){
-
+            if(this.currentMovingDirection == MovingDirection.down){
+                this.currentMovingDirection = MovingDirection.up;
+                this.requestMovingDirection = MovingDirection.up;
+            }
         }
         //down
         if(event.keyCode == 40){
@@ -64,7 +67,7 @@ draw(ctx) {
         }
         //right
         if(event.keyCode == 39){
-            
+
         }
     }
 }
